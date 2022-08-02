fd6d591094b36204a0a0bd3a7bbf92983bfd361b
bru-correa <bruuh.cor@gmail.com>
2022-08-02 19:51:44 -0300

    Send print colors to a different file

---
+++ src/print_colors.c
@@ -0,0 +1,16 @@
+#include "monitoring.h"
+
+void	print_in_blue(char *message)
+{
+	printf("\033[34;1m%s\033[0m ", message);
+}
+
+void	print_in_red(char *message)
+{
+	printf("\033[0;31m%s\033[0m\n", message);
+}
+
+void	print_in_green(char *message)
+{
+	printf("\033[0;32m%s\033[0m\n", message);
+}
--- src/print_simple.c
+++ src/print_simple.c
@@ -29,18 +29,3 @@ static void	print_simple_head(t_request *request, char *time)
 	print_in_blue("Date:");
 	printf("%s\n", time);
 }
-
-void	print_in_blue(char *message)
-{
-	printf("\033[34;1m%s\033[0m ", message);
-}
-
-void	print_in_red(char *message)
-{
-	printf("\033[0;31m%s\033[0m\n", message);
-}
-
-void	print_in_green(char *message)
-{
-	printf("\033[0;32m%s\033[0m\n", message);
-}
